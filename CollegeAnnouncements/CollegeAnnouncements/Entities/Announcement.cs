730dce0efc40f2d875dbbff055c506b7c724ae0c
mihailotomi <mihailotomi2@gmail.com>
2024-09-13 22:47:21 +0200

    chore: add announcements domain

---
+++ CollegeAnnouncements/CollegeAnnouncements/Entities/Announcement.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+
+namespace CollegeAnnouncements.Entities;
+
+
+#region Enums
+public enum AnnouncementPriorityType
+{
+    [Description("Normal")]
+    NORMAL,
+    [Description("High")]
+    HIGH,
+    [Description("Critical")]
+    CRITICAL,
+}
+
+public enum AnnouncementAudienceType
+{
+    [Description("Students")]
+    STUDENTS,
+    [Description("Employees")]
+    EMPLOYEES
+}
+#endregion
+
+public class Announcement
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string Content { get; private set; }
+    public DateTime ValidUntil { get; private set; }
+    public AnnouncementPriorityType Priority { get; private set; }
+    public List<AnnouncementAudienceType> Audience { get; private set; }
+    public List<Document> Documents { get; private set; }
+
+
+    protected Announcement(
+        int id,
+        string title,
+        string content,
+        DateTime validUntil,
+        AnnouncementPriorityType priority,
+        List<AnnouncementAudienceType> audience)
+    {
+        Id = id;
+        Title = title;
+        Content = content;
+        ValidUntil = validUntil;
+        Priority = priority;
+        Audience = audience;
+        Documents = [];
+    }
+
+    public static Announcement Create(
+        int id,
+        string title,
+        string content,
+        DateTime validUntil,
+        AnnouncementPriorityType priority,
+        List<AnnouncementAudienceType> audience)
+    {
+        return new Announcement(id, title, content, validUntil, priority, audience);
+    }
+
+    public Announcement AttachDocument(Document document)
+    {
+        Documents.Add(document);
+        return this;
+    }
+
+    public Announcement AttachDocumentRange(List<Document> documents)
+    {
+        Documents.AddRange(documents);
+        return this;
+    }
+}
+
---
+++ CollegeAnnouncements/CollegeAnnouncements/Entities/Document.cs
@@ -0,0 +1,32 @@
+﻿namespace CollegeAnnouncements.Entities;
+
+public class Document
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string FileUri { get; private set; }
+    public int AnnouncementId { get; private set; }
+    public Announcement Announcement { get; private set; } = null!;
+
+    protected Document(
+        int id,
+        string name,
+        string fileUri,
+        int announcementId)
+    {
+        Id = id;
+        Name = name;
+        FileUri = fileUri;
+        AnnouncementId = announcementId;
+    }
+
+    public static Document Create(
+        int id,
+        string name,
+        string fileUri,
+        int announcementId)
+    {
+        return new Document(id, name, fileUri, announcementId);
+    }
+}
+
