de8cdbfd3ea24a784f94a616064675182cb6ea5f
mihailotomi <mihailotomi2@gmail.com>
2024-09-15 20:02:36 +0200

    chore: add jwt validation

--- CollegeAnnouncements/CollegeAnnouncements/Program.cs
+++ CollegeAnnouncements/CollegeAnnouncements/Program.cs
@@ -1,16 +1,58 @@
 using CollegeAnnouncements.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 {
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+        // Define the security scheme for Bearer Token authentication
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Enter 'Bearer' [space] and then your valid token."
+        });
+
+        // Apply this security scheme to all operations (global security requirement)
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] {}
+            }
+        });
+    });
+
     builder.Services.AddInfrastructureServices(options =>
     {
         options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     });
 
-    builder.Logging.AddConsole();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        builder.Configuration.Bind(nameof(JwtBearerOptions), options);
+    });
+
+    builder.Services.AddAuthorization();
+
+
+
 }
 
 {
@@ -22,6 +64,9 @@ var builder = WebApplication.CreateBuilder(args);
         app.UseSwaggerUI();
     }
 
+    app.UseAuthentication();
+    app.UseAuthorization();
+
     app.UseHttpsRedirection();
 
     app.MapGet("/hello", () =>
@@ -29,6 +74,7 @@ var builder = WebApplication.CreateBuilder(args);
         return "Hello World";
     })
     .WithName("Hello")
+    .RequireAuthorization()
     .WithOpenApi();
 
     app.Run();
